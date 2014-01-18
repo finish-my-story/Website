f0b8132299d84971cc26bd1535ebdda0c0f7aafd
Shu <shudas@umich.edu>
2014-01-18 08:50:59 -0500

    almost have timer working

--- public/javascript/default.js
+++ public/javascript/default.js
@@ -1,6 +1,35 @@
  $(document).ready(function() {
  	var storyPadding = 60;
  	var newheight = $(window).outerHeight() - $(".topbar p").outerHeight(true) - $(".textContent").outerHeight(true) - storyPadding;
-    $(".storyContent").height(newheight);
-    console.log($(window).outerHeight());
-});	
+ 	var progressMargin = $(".leftBlock").outerWidth();
+    $(".boxContent").height(newheight);
+    $(".progressbar").css("margin-left", progressMargin);
+    $(".progressbar").width($("#userContent").width);
+    console.log($(window).outerWidth());
+    console.log($(".leftBlock").outerWidth(true));
+    console.log(parseInt($("body").css('margin-left').replace("px", "")));
+});
+
+var timer = 0;
+var perc = 0;
+
+function updateProgress(percentage) {
+    $('#pbar_innerdiv').css("width", percentage + "%");
+    $('#pbar_innertext').text(percentage + "%");
+}
+
+function animateUpdate() {
+    perc++;
+    updateProgress(perc);
+    if(perc < 100) {
+        timer = setTimeout(animateUpdate, 50);
+    }
+}
+
+$(document).ready(function() {
+    $('#pbar_outerdiv').mouseenter(function() {
+        clearTimeout(timer);
+        perc = 0;
+        animateUpdate();
+	});
+});
