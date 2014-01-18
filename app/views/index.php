ef90c232a23653fde1841ba8a5d82274a13b4498
Shu <shudas@umich.edu>
2014-01-18 02:06:04 -0500

    front page more or less done

--- app/views/index.php
+++ app/views/index.php
@@ -7,13 +7,21 @@
 </head>
 <body>
 	<div class="topbar">
-		<img src="assets/logo_large.png" alt="finish my story" height="88px"></img>
+		<p><span class="darkOrg">f</span>inish 
+			<span class="darkOrg">m</span class="darkOrg">y 
+			<span class="darkOrg">s</span>tory
+			<span class="darkOrg blink">|<span>
+		</p>
+		<!-- <img src="assets/logo_large.png" alt="finish my story" height="88px"></img> -->
 	</div>
 	<div class="ops">
-		<br/><br/>
 		<a href="#">Play</a>
-		<span style="color: #af66d5;">|</span>
+		<span>|</span>
 		<a href="#">Read</a>
+	</div>
+	<div class="desc">
+		<br/><br/><br/>
+		A story is waiting to be written...
 	</div>
 </body>
 </html>
