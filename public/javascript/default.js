f42ce4c423b6b427d6e9b7ad1d9e2ede3c24fce5
Shu <shudas@umich.edu>
2014-01-18 08:59:07 -0500

    shus last commit unless we decide to continue

--- public/javascript/default.js
+++ public/javascript/default.js
@@ -1,35 +1,32 @@
+var timer = 0;
+var perc = 0;
+
  $(document).ready(function() {
  	var storyPadding = 60;
  	var newheight = $(window).outerHeight() - $(".topbar p").outerHeight(true) - $(".textContent").outerHeight(true) - storyPadding;
  	var progressMargin = $(".leftBlock").outerWidth();
     $(".boxContent").height(newheight);
-    $(".progressbar").css("margin-left", progressMargin);
-    $(".progressbar").width($("#userContent").width);
+    $(".prog_outerdiv").css("margin-left", progressMargin);
+    $(".prog_outerdiv").width($("#userContent").width);
     console.log($(window).outerWidth());
     console.log($(".leftBlock").outerWidth(true));
     console.log(parseInt($("body").css('margin-left').replace("px", "")));
-});
-
-var timer = 0;
-var perc = 0;
-
-function updateProgress(percentage) {
-    $('#pbar_innerdiv').css("width", percentage + "%");
-    $('#pbar_innertext').text(percentage + "%");
-}
-
-function animateUpdate() {
-    perc++;
-    updateProgress(perc);
-    if(perc < 100) {
-        timer = setTimeout(animateUpdate, 50);
-    }
-}
-
-$(document).ready(function() {
-    $('#pbar_outerdiv').mouseenter(function() {
+    $('#prog_outerdiv').mouseenter(function() {
         clearTimeout(timer);
         perc = 0;
         animateUpdate();
 	});
 });
+
+function updateProgress(percentage) {
+    $('.prog_innerdiv').css("width", percentage + "%");
+    // $('#pbar_innertext').text(percentage + "%");
+}
+
+function animateUpdate() {
+    ++perc;
+    updateProgress(perc);
+    if(perc < 100) {
+        timer = setTimeout(animateUpdate, 50);
+    }
+}
